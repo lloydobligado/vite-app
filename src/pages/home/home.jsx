13c8afaf982c9a2940965af050dcf30e40668f96
Lloyd Carie M. Obligado <lloydobligado@gmail.com>
2023-09-19 19:23:44 +0800

    test: framer motion

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <motion.div animate={{x:100}}>HomePage</motion.div>;
 };
 
 export default HomePage;
