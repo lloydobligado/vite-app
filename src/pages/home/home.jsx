14a1b39d0f5208d84382bbcf7d74ddc5a4c608d5
Lloyd Carie M. Obligado <lloydobligado@gmail.com>
2023-09-19 20:09:21 +0800

    test: framer motion

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const HomePage = () => {
   return (
-    <div className='h-[100vh] flex justify-center items-center'>
+    <div className='h-[90vh] flex justify-center items-center'>
       <motion.div className="bg-red-500 w-[100px] h-[100px]" animate={{y:-100, scale: 1}} initial={{scale: 0}}></motion.div>
     </div>
   )
